c0fd21e23b769052ac19cfe41493a9791702bf73
yhr0x43 <yhr0x43@gmail.com>
2025-03-03 22:54:14 -0600

    glm::i16vec3 formatter

--- include/vks/Utils.hpp
+++ include/vks/Utils.hpp
@@ -2,20 +2,22 @@
 
 #include <string>
 
+#include <glm/glm.hpp>
+
 #include <vulkan/vulkan.h>
 #include <spdlog/spdlog.h>
 
 #if !defined(__PRETTY_FUNCTION__)
 #define __PRETTY_FUNCTION__ __FUNCSIG__
-#endif // _MSC_VER
+#endif
 
-#define VK_CHK(x) do {                                           \
-VkResult ret = (x);                                              \
-if (VK_SUCCESS != ret) {                                         \
-	spdlog::critical("\"{}\" results {} in {}",                  \
-		#x, vks::utils::statusString(ret), __PRETTY_FUNCTION__); \
-	assert(ret == VK_SUCCESS);                                   \
-} } while(0)                                                     \
+#define VK_CHK(x) do {                                                  \
+        VkResult ret = (x);                                             \
+        if (VK_SUCCESS != ret) {                                        \
+            spdlog::critical("\"{}\" results {} in {}",                 \
+                             #x, vks::utils::statusString(ret), __PRETTY_FUNCTION__); \
+            assert(ret == VK_SUCCESS);                                  \
+        } } while(0)                                                    \
 
 #define VK_FLAGS_NONE 0
 #define DEFAULT_FENCE_TIMEOUT 100000000000
@@ -33,3 +35,12 @@ void exitFatal(const std::string& message, VkResult resultCode);
 VkShaderModule loadShader(const char* fileName, VkDevice device);
 }
 }
+
+template<>
+struct fmt::formatter<glm::i16vec3> : fmt::formatter<std::string>
+{
+    auto format(glm::i16vec3 obj, format_context &ctx) const -> decltype(ctx.out())
+    {
+        return fmt::format_to(ctx.out(), "({},{},{})", obj.x, obj.y, obj.z);
+    }
+};
