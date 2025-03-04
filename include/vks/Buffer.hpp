09b65d0ee67905e2594ecf183243bc767f5f763f
yhr0x43 <yhr0x43@gmail.com>
2025-03-03 22:54:25 -0600

    indent cleanup

--- include/vks/Buffer.hpp
+++ include/vks/Buffer.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <optional>
 
 #include <vulkan/vulkan.h>
 
@@ -10,26 +11,26 @@ class Device;
 
 class Buffer : public VulkanEncapsulate<VkBuffer>
 {
-	Device const& m_Device;
+    Device const& m_Device;
 
-	VkDeviceMemory m_Memory;
-	VkDeviceSize m_Size;
-	void* m_pMapped;
+    VkDeviceMemory m_Memory;
+    VkDeviceSize m_Size;
+    void* m_pMapped;
 
 public:
-	VkResult Flush(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0) const noexcept;
-	VkResult Map(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0, VkMemoryMapFlags flags = 0) noexcept;
-	void Unmap(void) noexcept;
-	void CopyData(const void* data, size_t size) noexcept;
-	void CopyFrom(Buffer& src, std::optional<VkBufferCopy> bufferCopy = std::nullopt) const noexcept;
+    VkResult Flush(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0) const noexcept;
+    VkResult Map(VkDeviceSize size = VK_WHOLE_SIZE, VkDeviceSize offset = 0, VkMemoryMapFlags flags = 0) noexcept;
+    void Unmap(void) noexcept;
+    void CopyData(const void* data, size_t size) noexcept;
+    void CopyFrom(Buffer& src, std::optional<VkBufferCopy> bufferCopy = std::nullopt) const noexcept;
 
-	Buffer(
-		Device const& device,
-		VkBufferUsageFlags usageFlags,
-		VkMemoryPropertyFlags memoryPropertyFlags,
-		VkDeviceSize size,
-		void* data = nullptr
-	) noexcept;
-	~Buffer(void) noexcept;
+    Buffer(
+        Device const& device,
+        VkBufferUsageFlags usageFlags,
+        VkMemoryPropertyFlags memoryPropertyFlags,
+        VkDeviceSize size,
+        void* data = nullptr
+        ) noexcept;
+    ~Buffer(void) noexcept;
 };
 }
--- include/vks/VulkanEncapsulate.hpp
+++ include/vks/VulkanEncapsulate.hpp
@@ -7,23 +7,23 @@ namespace vks
 class NonCopyable
 {
 protected:
-	NonCopyable(void) = default;
-	~NonCopyable(void) = default;
+    NonCopyable(void) = default;
+    ~NonCopyable(void) = default;
 
-	NonCopyable(NonCopyable const&) = delete;
-	void operator=(NonCopyable const&) = delete;
+    NonCopyable(NonCopyable const&) = delete;
+    void operator=(NonCopyable const&) = delete;
 };
 
 template<class T>
 class VulkanEncapsulate : public NonCopyable
 {
 protected:
-	T m_Handle{ VK_NULL_HANDLE };
+    T m_Handle{ VK_NULL_HANDLE };
 
 public:
-	operator T() const
-	{
-		return m_Handle;
-	};
+    operator T() const
+    {
+        return m_Handle;
+    };
 };
 }
